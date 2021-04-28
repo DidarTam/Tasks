69ec1e2a7559d1579f66af00f89a1849fc140830
DidarTam <83034019+DidarTam@users.noreply.github.com>
2021-04-28 09:49:15 +0600

    Add files via upload

---
+++ AJAX/index.js
@@ -0,0 +1,15 @@
+//const resultBlock = document.querySelector('#result');
+//condt clickMeButtom = document / querySelector('#click-me');
+//clickMeButton.addEventListener("click", makeRequest);
+
+//function makeRequest() {
+//	resultBlock.innerHTML - 'result';
+//}
+
+var a = 5;
+
+$.ajax('https://repetitora.net/api/JS/Images');
+
+a = 8;
+
+console.log(a);
